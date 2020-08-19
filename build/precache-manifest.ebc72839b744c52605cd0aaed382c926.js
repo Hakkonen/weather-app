2824bcb86a1beb899bf482ed1e7b5cb234f4a13c
hakkonen <rnlds.j@gmail.com>
2020-08-20 08:29:33 +1000

    Upload dependancies

---
+++ build/precache-manifest.ebc72839b744c52605cd0aaed382c926.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "070e66e016e794509b6107c06f74f12d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d133bafbdf7c6c489a3",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "627d50b6360739dafd39",
+    "url": "/static/css/main.03eabdb8.chunk.css"
+  },
+  {
+    "revision": "7d133bafbdf7c6c489a3",
+    "url": "/static/js/2.6753d7b2.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/static/js/2.6753d7b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "627d50b6360739dafd39",
+    "url": "/static/js/main.6d0577fa.chunk.js"
+  },
+  {
+    "revision": "f294869ae1697dfe421d",
+    "url": "/static/js/runtime-main.7ecb0647.js"
+  }
+]);
